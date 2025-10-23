8a8f556f62bdeab55f166b9a509ac8bb0e32bbe7
ISudhan <sudhann001@gmail.com>
2025-10-24 05:00:13 +0530

    svg added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+import ServicesAndSupport from './components/ServicesAndSupport';
 
+function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-    </>
-  )
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/services" element={<ServicesAndSupport/>} />
+        {/* Add other routes */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
